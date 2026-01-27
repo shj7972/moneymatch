26dfbb1bdceec1aa1f21182aef8a47d24c6c9b8d
Seohy <seohy@example.com>
2026-01-27 21:10:15 +0900

    Update robots.txt to explicitly allow Yeti

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,16 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-        },
+        rules: [
+            {
+                userAgent: 'Yeti',
+                allow: '/',
+            },
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+        ],
         sitemap: 'https://moneymatch.kr/sitemap.xml',
     }
 }
