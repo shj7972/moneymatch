c62d8f5512b9665c164cfb4109e4974ee37b21ca
Seohy <seohy@example.com>
2026-01-27 21:43:04 +0900

    Add Google AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,12 @@ export default function RootLayout({
         <html lang="ko">
             <body className={inter.className}>
                 <Script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2947913248390883"
+                    crossOrigin="anonymous"
+                    strategy="afterInteractive"
+                />
+                <Script
                     src="https://www.googletagmanager.com/gtag/js?id=G-W73M3RBDFT"
                     strategy="afterInteractive"
                 />
